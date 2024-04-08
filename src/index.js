78c2ebfe2274255ca171fc4d79d022e0648afd3f
Wajid Ali Hashmi <wajidalionline001@gmail.com>
2024-04-08 19:38:01 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/login', (req, res) => {
-    res.send('<h1>Chapter Login</h1>')
-})
-app.get('/About', (req, res) =>{
-    res.send('<h1>About</h1>')
-})
-app.get('/Contact', (req, res)=>{
-    res.send('<h1>Contact</h1>')
-})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
